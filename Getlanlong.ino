436a524766d677063dc8fe4c1d2b6bf72e7885b7
mhhabib <mhrahman7096@gmail.com>
2019-03-22 22:34:56 +0600

    Get only Lat Long

---
+++ Getlanlong.ino
@@ -0,0 +1,24 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+void setup(){
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+}
+void loop(){
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      // Latitude in degrees (double)
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);      
+      // Longitude in degrees (double)
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6); 
+    }
+  }
+}
